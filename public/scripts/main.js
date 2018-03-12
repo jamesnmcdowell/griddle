2f5af801a686680b4b1194b4ff35cfe68f00316b
James McDowell <jamesnmcdowell@gmail.com>
2018-03-12 13:41:35 -0400

    setup firebase hosting for application

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,7 +6,21 @@ document.addEventListener('DOMContentLoaded', function (event) {
   //get data from firebase and render on page
   getData().then(render).then(swappable);
   
+  // The Firebase SDK is initialized and available here!
+  //
+  // firebase.auth().onAuthStateChanged(user => { });
+  // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+  // firebase.messaging().requestPermission().then(() => { });
+  // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
   
+  try {
+    let app = firebase.app();
+    let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
+    document.getElementById('load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
+  } catch (e) {
+    console.error(e);
+    document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
+  }
 });
 //draggable JS library
 let swappable = function() {
